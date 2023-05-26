123dc98c048f02ea042733b93782e6dc0f7b65f2
Anuj kumar <trigbit.anuj@gmail.com>
2023-05-26 18:23:26 +0530

    Create page.tsx

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+export const metadata = {
+  title: "contact page",
+  description: "geet in contact",
+};
+const page = () => {
+  return <div>Contact Page</div>;
+};
+
+export default page;
