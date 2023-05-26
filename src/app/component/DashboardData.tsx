e7b9be3c04f5ce4f4c766e4c09541bfdfcc6654e
Anuj kumar <trigbit.anuj@gmail.com>
2023-05-26 18:23:04 +0530

    Create DashboardData.tsx

---
+++ src/app/component/DashboardData.tsx
@@ -0,0 +1,34 @@
+type PromiseData = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+const DashboardData = async () => {
+  // const { data } = await axios.get(
+  //   "https://jsonplaceholder.typicode.com/todos/1"
+  // );
+  const id = Math.floor(Math.random() * 10 + 1);
+
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
+
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+    cache: "no-cache",
+  });
+  if (!res.ok) {
+    throw new Error("can not fetch data");
+  }
+  const data = (await res.json()) as PromiseData;
+
+  return (
+    <div>
+      <h1>{data.id}</h1>
+      <p>{data.title}</p>
+    </div>
+  );
+};
+
+export default DashboardData;
